049dcfb4a9ad54d25a7a98d8d9fd22bf1804d254
Miguel Ben <itsmigben@gmail.com>
2019-12-09 19:20:48 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -20,6 +20,8 @@ def assembled_matrix
   # Build an array that contains (or, "nests") the residents of The Ennet House
   # and the Enfield Tennis Academy as provided by the constants
   
+  #------------------------------------------------------------
+  
   # Failure/Error: expect(assembled_matrix).to eq([ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY])
   
   [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
