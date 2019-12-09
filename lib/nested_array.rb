2a10526187ed1f33f1f36f6ef4aa3b466c8b07f9
Miguel Ben <itsmigben@gmail.com>
2019-12-09 19:25:44 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -24,7 +24,8 @@ def assembled_matrix
   
   # Failure/Error: expect(assembled_matrix).to eq([ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY])
   
-  [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
+  [ ENNET_HOUSE, 
+    ENFIELD_TENNIS_ACADEMY]
   # 3/4 Failures
 end
 
